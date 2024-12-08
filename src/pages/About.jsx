1839c0f78eb5bb709d293166717e5b3fc6252d30
tyler <ruiztyler24@gmail.com>
2024-12-08 10:27:51 -0500

    added user validation with navigate

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react"
+import { Navigate } from "react-router-dom"
 
 
 const About = () => {
+    // BASIC USER VALIDATION AND TESTING NAVIGATE
+    // create a state for the user mario, he is automatically logged in
+    const [user, setUser] = useState('mario') 
+    // if mario isnt logged in send him to the home page
+    if (!user) {
+        return(
+            <Navigate to='/' />
+            //if we didnt want the user to navigate backwards though you type
+            // replace={true} and itll work
+        )
+    }
+    //if he is return this instead
     return (
         <div className="about">
             <h2>About</h2>
@@ -8,6 +22,8 @@ const About = () => {
                 Voluptate doloribus optio totam saepe aliquam, et soluta
                  expedita magnam molestiae odit. Eligendi, quis mollitia?
                   Iusto velit deleniti veritatis, autem quis voluptate.</p>
+            <button onClick={() => setUser(null)}>LogOut</button>
+            {/* simple button that will setuser to null to make the if statement above work */}
         </div>
     )
 }
